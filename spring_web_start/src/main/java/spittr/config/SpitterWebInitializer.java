684628e4dbd9c900b094d80d61a272fe761fcd39
zhankui.liang <zhankui.liang@51e.com.cn>
2020-12-18 16:15:26 +0800

    init

---
+++ spring_web_start/src/main/java/spittr/config/SpitterWebInitializer.java
@@ -0,0 +1,18 @@
+package spittr.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpitterWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{RootConfig.class};
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{WebConfig.class};
+    }
+}
