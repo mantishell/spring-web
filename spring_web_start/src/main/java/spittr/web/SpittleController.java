d3c2be93fe48daf9ab94dc03d9ead3f70bc2ccb2
zhankui.liang <zhankui.liang@51e.com.cn>
2020-12-22 08:28:48 +0800

    Model

--- spring_web_start/src/main/java/spittr/web/SpittleController.java
+++ spring_web_start/src/main/java/spittr/web/SpittleController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import spittr.data.SpittleRepository;
 
+import java.util.Map;
+
 @Controller
 @RequestMapping("/spittles")
 public class SpittleController {
@@ -19,8 +21,23 @@ public class SpittleController {
     }
     @RequestMapping(method = RequestMethod.GET)
     public String spittles(Model model){
+        /**
+         * 1、当调用 addAttribute() 方法并且不指定 key 的时候，那么 key 会根据值的对象类型推断确定。在本例中，因为它是一个 List<Spittle>，因此，键将会推断为 spittleList。
+         */
         //model.addAttribute(spittleRepository.findSpittles(Long.MAX_VALUE, 20));
+        /**
+         * 2、显式声明模型的 key
+         */
         model.addAttribute("spittleList", spittleRepository.findSpittles(Long.MAX_VALUE, 20));
         return "spittles";
     }
+
+    /**
+     * 3、如果你希望使用非 Spring 类型的话，那么可以用 java.util.Map 来代替 Model
+     */
+    /*@RequestMapping(method = RequestMethod.GET)
+    public String spittles(Map model){
+        model.put("splittleList", spittleRepository.findSpittles(Long.MAX_VALUE, 20));
+        return "spittles";
+    }*/
 }
