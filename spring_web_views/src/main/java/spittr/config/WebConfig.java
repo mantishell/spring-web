71abccbfa6f2bd3cb56af9641517a4bd74177fe1
mantishell <47586.73703.?a>
2020-12-27 14:59:37 +0800

    添加thymeleaf视图模板

---
+++ spring_web_views/src/main/java/spittr/config/WebConfig.java
@@ -0,0 +1,65 @@
+package spittr.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("spittr.web")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    /**
+     * 配置视图解析器
+     * 将逻辑视图名称解析为 Thymeleaf 模板视图
+     */
+    @Bean
+    public ViewResolver viewResolver(SpringTemplateEngine templateEngine) {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine);
+        return viewResolver;
+    }
+
+    /**
+     * 处理模板并渲染结果
+     */
+    @Bean
+    public SpringTemplateEngine templateEngine(ITemplateResolver templateResolver) {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        return templateEngine;
+    }
+
+    /**
+     * 加载 Thymeleaf 模板
+     */
+    @Bean
+    public ITemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("/WEB-INF/views/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+        return templateResolver;
+    }
+
+    /**
+     *  DispatcherServlet 将对静态资源的请求转发到 Servlet 容器中默认的 Servlet 上，
+     *  而不是使用 DispatcherServlet 本身来处理此类请求。
+     * @param configurer
+     */
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+}
